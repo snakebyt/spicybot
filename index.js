dad9bd87e4efe2dd487b26a03a2178b614e12c70
Lee Martin <lee.martin@pliminus.co.uk>
2016-12-23 22:33:36 +0000

    version 0.1.0

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 //Spicybot, your friendly spicypixel replacement
 
-const irc = require('irc');
+var irc = require('irc');
 const fs = require('fs');
 
 const config = {
@@ -13,34 +13,38 @@ var spicybot = new irc.Client(config.server, config.botName, {
 	channels : config.channels
 });
 
-spicybot.prototype.masturbate = function(){
-	grip = Math.floor(Math.random()*6)+1;
+spicybot.masturbate = function(){
+	grip = Math.floor(Math.random()*10)+5;
 	lube = grip*1000;
 	smeg = lube*60;
 	return smeg;
 };
 
-spicybot.prototype.seeIfMissed = function(initMsg){
-	fs.readFile('curMsg.txt', (err, curMsg) => {
+spicybot.ifQuiet = function(initMsg){
+	fs.readFile('curMsg.txt', 'utf8',(err, curMsg) => {
 		if (err) throw err;
 		if (curMsg === initMsg){
 			spicybot.snark();
 		}else{
 			//sulk
 		}
-	}
+	});
 };
 
-spicybot.prototype.snark = function(){
+spicybot.snark = function(){
 	debate = Math.floor(Math.random()*(10));
 	snarkResponse = ['yup', 'yep', 'mhm', 'mhmmm', 'cute', 'nice', 'yay', 'yaaay', 'ayup', 'yah'];
 	spicybot.say(config.channels[0], snarkResponse[debate]);
-
+	var messagetime = new Date().toISOString();
+	console.log(messagetime + ': spicybot said ' + snarkResponse[debate]);
 };
 
 spicybot.addListener('message', function(from, to, text, message) {
-	fs.writeFile('curMsg.txt', message, (err) => {
+	var inMsg = text;
+	fs.writeFile('curMsg.txt', inMsg, (err) => {
 		if (err) throw err;
-		setTimeout( spicybot.seeIfMissed(message), spicybot.masturbate());
+		var _this = this;
+		setTimeout( function(){_this.ifQuiet(inMsg);}, spicybot.masturbate());
 	});
+	return;
 });
